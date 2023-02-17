6eb0daa7cbd7134657ececdd80dbdafb87cacc90
Deepeshtq <tq9439434185@gmail.com>
2023-02-17 18:13:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Componentes/Navbar";
+import TextForm from "./Componentes/TextForm";
+// import About from "./Componentes/About";
+import React ,{useState} from 'react'
+import Alert from "./Componentes/Alert";
+
+function App() {
+  const [mode, setmode] = useState('light'); //wheather dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message , type)=>{
+    setAlert({
+      msg : message,
+      type:type
+    });
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+
+  }
+
+
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setmode('dark');
+      document.body.style.background=`grey`;
+      showAlert("Dark mode has been enabled","Success");
+    }
+    else{
+      setmode('light');
+      document.body.style.background=`white`;
+      showAlert("Light mode has been enabled" ,"Success");
+
+    }
+  }
+  return (
+    <>
+      <Navbar title="TextUtls" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
+      <Alert alert ={alert} />
+      {/* <Navbar title ={3} aboutText="About Us"/> */}
+      {/* <Navbar/> */}
+      <div className="container">
+      <TextForm  showAlert={showAlert} heading="Enter the text to analyze "  mode={mode}/>
+      {/* <About/> */}
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Componentes/TextForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("uppercase is clicked ");
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase","Success");
+  }
+
+  const handleLoClick = () => {
+    // console.log("uppercase is clicked ");
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase","Success");
+  }
+
+  const handleClear =()=>{
+    // console.log("clear is clicked");
+    let newText="";
+    setText(newText);
+    props.showAlert("Text Cleared","Success");
+  }
+
+  const handleOnChange = (event) => {
+    // console.log("onchange");
+    setText(event.target.value);
+  }
+  
+  const handleCopy = ()=>{
+    let text =document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to clipbored","Success");
+  }
+
+  const handleExtraSpace =()=>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+    props.showAlert("Extra Space Removed","Success");
+  }
+
+  const [text, setText] = useState("");
+  // text = "new text"; Worng way to change the state
+  // setText("new text");
+  return (
+    <>
+      <div>
+        <div className="mb-3" style={{color:props.mode===`dark`?`white`:`black`}}>
+          <h1>{props.heading}</h1>
+          <textarea className="form-control" value={text} style={{backgroundColor:props.mode===`dark`?`grey`:`white`,color:props.mode===`dark`?`white`:`black` }} onChange={handleOnChange} id="myBox" rows={8} />
+        </div>
+        <button className='btn btn-primary ' onClick={handleUpClick}>Convert To UpperCase</button>
+        <button className='btn btn-primary mx-1' onClick={handleLoClick}>Convert To LowerCase</button>
+        <button className='btn btn-primary mx-1 ' onClick={handleClear}>Clear</button>
+        <button className='btn btn-primary mx-1' onClick={handleCopy}>Copy Text</button>
+        <button className='btn btn-primary mx-1' onClick={handleExtraSpace}>Remove Spaces</button>
+      </div>
+
+      <div className="container"  style={{color:props.mode===`dark`?`white`:`black`}}>
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length} Words and {text.length}</p>
+        <p>{0.008* text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:'Enter text to preview it'}</p>
+      </div>
+    </>
+  )
+}
